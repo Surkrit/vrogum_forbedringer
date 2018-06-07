5a5886e43ed7c2ed819b70d59a690aecdcc60d29
Surkrit <jensendennis520@gmail.com>
2018-06-07 13:06:19 +0200

    alt for meget siden sidste push....

--- assets/js/script5.js
+++ assets/js/script5.js
@@ -23,3 +23,20 @@ function showDivs(n) {
 */
 
 /* Slider next/previous slut */
+
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 480 || document.documentElement.scrollTop > 480) {
+        document.getElementById("backtoTop").style.display = "block";
+    } else {
+        document.getElementById("backtoTop").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} */
+
+document.getElementById("favIcon").classList.toggle('fas fa-heart');
+document.getElementById("favIcon").classList.toggle('far fa-heart');
